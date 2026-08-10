bb58bb0c77b1af39516dfd1fa1657ff0e699458b
Sakshi Rautela <user@sakshirautela.local>
2026-08-10 12:10:12 +0530

    initialize project folder structure

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-const socket=new WebSocket('')
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '127.0.0.1', () => { 
+  console.log(`Server running on port ${PORT}`);
+ });
