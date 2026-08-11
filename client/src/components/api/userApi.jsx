744204081b928135df06d3b8916f0af37c192fe3
sakshirautela-af <sakshi.rautela@appfoster.com>
2026-08-11 11:21:23 +0530

    design and style new signup page

---
+++ client/src/components/api/userApi.jsx
@@ -0,0 +1,20 @@
+const api=import.meta.env.VITE_API_URL
+export const getUserDetails=async(userId)=>{
+   const response= await fetch(`${api}/users/${userId}`,{
+    method:'GET'
+   })
+   const user=await response.json()
+   return user
+}
+
+export const createUserDetails=async(userData)=>{
+   const response= await fetch(`${api}/users/create`,{
+    method:'POST',
+    headers:{
+         "Content-Type": "application/json"
+    },
+    body: JSON.stringify(userData)
+   })
+   const user=await response.json()
+   return user
+}
