684e69a387c03cb1dda038326d3c66b0678b16c6
sakshirautela-af <sakshi.rautela@appfoster.com>
2026-08-20 19:28:24 +0530

    updated the endpit

--- server/src/index.js
+++ server/src/index.js
@@ -17,12 +17,12 @@ app.set("io", io);
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
-app.use("users", userRoutes);
-app.use("projects", projectRoutes);
-app.use("tasks", taskRoutes);
-app.use("util", otherRoutes);
-app.use("files", fileRoutes);
-app.use("notifications", notificationRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/util", otherRoutes);
+app.use("/api/files", fileRoutes);
+app.use("/api/notifications", notificationRoutes);
 app.use((err, req, res, next) => {
   console.error("Unhandled error:", err);
   res.status(err.status || 500).json({
