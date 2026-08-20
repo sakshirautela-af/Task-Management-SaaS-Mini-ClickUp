92c807970d3d82ceb96714c048fd58af385e8b51
sakshirautela-af <sakshi.rautela@appfoster.com>
2026-08-20 15:47:58 +0530

    updated prisma for postgreSql

--- server/src/config/prisma.js
+++ server/src/config/prisma.js
@@ -1,14 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import "dotenv/config";
-const databaseUrl = new URL(process.env.DATABASE_URL || "");
-const adapter = new PrismaMariaDb({
-  host: databaseUrl.hostname,
-  port: parseInt(databaseUrl.port || "3306", 10),
-  user: databaseUrl.username,
-  password: databaseUrl.password,
-  database: databaseUrl.pathname.slice(1),
-  connectionLimit: 10,
-});
-export const prisma = new PrismaClient({ adapter });
+
+export const prisma = new PrismaClient();
+
 export default prisma;
